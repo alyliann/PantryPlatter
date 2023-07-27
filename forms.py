bce19a70a578bbfe80a54b6f23ee4cc5c292e476
Alysa Vega <alyliann0325@gmail.com>
2023-07-27 18:28:40 +0000

    Fixed styles in forms.py

--- forms.py
+++ forms.py
@@ -2,22 +2,27 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, EmailField
 from wtforms.validators import DataRequired, Length, Email, EqualTo
 
+
 class SignUpForm(FlaskForm):
     name = StringField('Name',
                        validators=[DataRequired(), Length(min=2, max=20)])
     email = EmailField('Email',
-                        validators=[DataRequired(), Email()])
-    password = PasswordField('Password', validators=[DataRequired(), Length(min=8)])
+                       validators=[DataRequired(), Email()])
+    password = PasswordField('Password',
+                             validators=[DataRequired(), Length(min=8)])
     confirm_password = PasswordField('Confirm Password',
-                                     validators=[DataRequired(), EqualTo('password')])
+                                     validators=[DataRequired(),
+                                                 EqualTo('password')])
     submit = SubmitField('Sign Up')
+
 
 class SignInForm(FlaskForm):
     email = EmailField('Email',
-                        validators=[DataRequired(), Email()])
+                       validators=[DataRequired(), Email()])
     password = PasswordField('Password', validators=[DataRequired()])
 
     submit = SubmitField('Sign In')
+
 
 class RecipeForm(FlaskForm):
     in_1 = StringField('Ingredient 01',
